7715856669e22abfe90ea8b0df8279efcd01cb2e
hjcg123 <94056440+hjc236@users.noreply.github.com>
2021-12-07 15:30:40 +0000

    readme updates

--- setup.py
+++ setup.py
@@ -8,19 +8,19 @@ setup(
     author = 'hjc236',
     author_email = '',
     url = 'https://github.com/hjc236/hjc236_covid_dashboard',
-    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-    keywords = ['coronavirus', 'event-driven', 'dashboard'],   # Keywords that define your package best
-    install_requires=[            # I get to this in a second
+    download_url = 'https://github.com/hjc236/hjc236_covid_dashboard/archive/refs/tags/v0.1.tar.gz',
+    keywords = ['coronavirus', 'event-driven', 'dashboard'],
+    install_requires=[
         'uk_covid19',
         'flask',
         'pytest'
 
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.10',
     ],
 )
