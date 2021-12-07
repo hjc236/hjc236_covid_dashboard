531c369f4d3a57f8c14dbeafcf010a73a74a392d
hjcg123 <94056440+hjc236@users.noreply.github.com>
2021-12-07 15:53:56 +0000

    readme updates

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from distutils.core import setup
+from pathlib import Path
+
+# Get README file and pass this so it displays on PyPi
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name = 'hjc236_covid_dashboard',
     packages = ['hjc236_covid_dashboard'],
-    version = '0.2.1',
+    version = '1.0',
     license='MIT',
     description = 'A python project which generates a web dashboard with COVID-19 data and relevant news articles.',
-    long_description="file: README.md",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author = 'hjc236',
     author_email = '',
     url = 'https://github.com/hjc236/hjc236_covid_dashboard',
-    download_url = 'https://github.com/hjc236/hjc236_covid_dashboard/archive/refs/tags/v0.2.1.tar.gz',
+    download_url = 'https://github.com/hjc236/hjc236_covid_dashboard/archive/refs/tags/v0.2.2.tar.gz',
     keywords = ['coronavirus', 'event-driven', 'dashboard'],
     install_requires=[
         'uk_covid19',
